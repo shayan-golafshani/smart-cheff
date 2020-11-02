65b15dc8125e7d47baa3c58fc3d26a1dac4303eb
Alex Garber <alexgarber6@gmail.com>
2020-11-02 07:42:08 -0700

    Builds with new dao and database classes

--- app/src/main/java/edu/cnm/deepdive/smartcheff/model/dao/RecipeDao.java
+++ app/src/main/java/edu/cnm/deepdive/smartcheff/model/dao/RecipeDao.java
@@ -1,8 +1,31 @@
 package edu.cnm.deepdive.smartcheff.model.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import edu.cnm.deepdive.smartcheff.model.entity.Recipe;
+import edu.cnm.deepdive.smartcheff.model.entity.RecipeIngredient;
+import io.reactivex.Single;
+import java.util.Collection;
+import java.util.List;
 
 @Dao
 public interface RecipeDao {
+  @Insert
+  Single<Long> insert(Recipe recipe);
 
+  @Insert
+  Single<List<Long>> insert(Recipe...recipes);
+
+  @Insert
+  Single<List<Long>> insert(Collection<Recipe> recipes);
+
+  @Delete
+  Single<Integer> delete(Recipe recipe);
+
+  @Delete
+  Single<Integer> delete(Recipe...recipes);
+
+  @Delete
+  Single<Integer>delete(Collection<Recipe> recipes);
 }
--- app/src/main/java/edu/cnm/deepdive/smartcheff/model/dao/RecipeIngredientDao.java
+++ app/src/main/java/edu/cnm/deepdive/smartcheff/model/dao/RecipeIngredientDao.java
@@ -1,8 +1,33 @@
 package edu.cnm.deepdive.smartcheff.model.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import edu.cnm.deepdive.smartcheff.model.entity.RecipeIngredient;
+import edu.cnm.deepdive.smartcheff.model.entity.User;
+import io.reactivex.Single;
+import java.util.Collection;
+import java.util.List;
 
 @Dao
 public interface RecipeIngredientDao {
+
+  @Insert
+  Single<Long> insert(RecipeIngredient recipeIngredient);
+
+  @Insert
+  Single<List<Long>> insert(RecipeIngredient...recipeIngredient);
+
+  @Insert
+  Single<List<Long>> insert(Collection<RecipeIngredient> recipeIngredient);
+
+  @Delete
+  Single<Integer> delete(RecipeIngredient recipeIngredient);
+
+  @Delete
+  Single<Integer> delete(RecipeIngredient...recipeIngredient);
+
+  @Delete
+  Single<Integer>delete(Collection<RecipeIngredient> recipeIngredient);
 
 }
